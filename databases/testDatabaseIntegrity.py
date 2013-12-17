1a3836421ab462efdce94e8e64a3ea2061dd7b54
davidjn <davidjn@stanford.edu>
2013-12-17 14:08:13 -0800

    Attempted new file...

---
+++ databases/testDatabaseIntegrity.py
@@ -0,0 +1,8 @@
+#################
+##
+## This will be used to build functions that test the integrity of our databases.
+#
+# Functions:
+#   - itemNumberCheck: checks that no db has the same item listed twice
+#   - itemTitleCheck:  checks to see how many items have the same title (relisted items ?)
+#   - characterCheck:  check uniform character usage, eg " = &quot;
