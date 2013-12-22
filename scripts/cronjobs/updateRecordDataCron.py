d566bb82a4265c7a89d6cb775ab71662878d2ce6
davidjn <davidjn@stanford.edu>
2013-12-21 16:43:44 -0800

    Added scripts directory, with cronjob scripts

---
+++ scripts/cronjobs/updateRecordDataCron.py
@@ -0,0 +1,37 @@
+import sys,os
+sys.path.insert(0, '%s/../../functions/' % os.path.dirname(os.path.abspath(__file__)))
+from dataScraping import *
+
+#########################################################################################################
+
+## Using this file requires that the eBay python API is already installed. The APPID, CERTID, and DEVID
+## global variables must be updated below with those found in the users eBay developer accounts.
+
+## Update the lines below with the information from your eBay developer account.
+APPID = 'DavidNic-a026-403b-ade6-36262dc260a3'
+CERTID = '625388f8-ad85-4536-88d8-256113d58703'
+DEVID = '8b39a15d-e703-47bf-a7e7-db027f79100d'
+
+## Will be important to check over word list and fix things like " = &quot;. 
+
+#########################################################################################################
+    
+def updateDb(db_file):
+    conn = sqlite3.connect(db_file)
+    db = conn.cursor()
+    qString = "SELECT * FROM training_set"
+    results = list(db.execute(qString))
+    for row in results:
+        if row[6]!='true':
+            updateSingleEntry(db,row[0])
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    updateDb('/home/davidjn/webapps/nbdb/guitar_set.db')
+    updateDb('/home/davidjn/webapps/nbdb/laptop_set.db')
+    updateDb('/home/davidjn/webapps/nbdb/record_set.db')
+
+    #updateDb('../../databases/guitar_set.db')
+    #updateDb('../../databases/laptop_set.db')   
+    #updateDb('../../databases/record_set.db')
